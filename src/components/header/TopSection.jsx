f490375f5ef6a5041492bf0c76c781da33fbc5fd
Assim <asimankon@gmail.com>
2025-08-02 01:00:22 +0600

    Edited homepage and updated styIes

--- src/components/header/TopSection.jsx
+++ src/components/header/TopSection.jsx
@@ -21,7 +21,7 @@ function TopSection() {
           <IoLogoYoutube size={25} className='text-[#ee4023] hover:text-sky-500 cursor-pointer' />
         </div>
 
-        {/* Logo - Always Visible */}
+        {/* Logo - Always Visible 
         <img className="w-[160px]" src="https://w7.pngwing.com/pngs/650/450/png-transparent-prothom-alo-%E0%A6%96%E0%A7%87%E0%A6%B2%E0%A6%BE-dhaka-bengali-sport-others-text-sport-logo.png" alt="" />
 
         {/* Subscribe Button - Always Visible */}
@@ -39,8 +39,8 @@ function TopSection() {
             <h6 className='text-[17px] text-black hover:text-[#ee4023]'>(+88) 01556 440078</h6>
           </div>
           <div className='flex items-center gap-2 cursor-pointer'>
-            <MdOutlineMail size={20} className='text-[#ee4023]' />
-            <h6 className='text-[17px] text-black hover:text-[#ee4023]'>contact@prothomalo.org</h6>
+            <MdOutlineMail size={20} className='text-[#ee4023]' />{/*contact@prothomalo.org*/}
+            <h6 className='text-[17px] text-black hover:text-[#ee4023]'>asimankon@gmail.com</h6>
           </div>
         </Link>
       </div>
