becc1f42aab3e3d70340fda89fca4158ae8b1fd7
Assim <asimankon@gmail.com>
2025-09-04 18:51:54 +0600

    Edited homepage and updated styIes

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -7,7 +7,7 @@ import Footer from "../Components/Footer/Footer";
 const Root = () => {
   return (
     <div>
-      <div className="xs:hidden lg:block">
+      <div className="hidden lg:block">
         <TopHeader />
       </div>
       <div className="sticky top-0 z-10">
