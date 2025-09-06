6301f5ab26fa19349452413244158b1543d0eebf
Assim <asimankon@gmail.com>
2025-09-07 03:00:58 +0600

    Edited homepage and updated styIes

--- src/Page/Contact Me Page/ContactMePage.jsx
+++ src/Page/Contact Me Page/ContactMePage.jsx
@@ -4,7 +4,7 @@ import ContactMe from "../Home/HomeComponents/ContactMe";
 const ContactMePage = () => {
     return (
         <>
-                    <div className='h-screen bg-gradient-to-l from-purple-50 to-sky-50'>
+                    <div className='lg:h-screen bg-gradient-to-l from-purple-50 to-sky-50'>
                         <ContactMe />
                     </div>
                 </>
