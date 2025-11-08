a4a4d71dd8a72080ae35a84726bde5df7b0b50e0
Assim <asimankon@gmail.com>
2025-11-08 13:28:12 +0600

    Edited homepage and updated styIes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     extend: {},
   },
