9bf1880118bb049456a9657733bb8d786dc9bad8
Asim <asimankon@gmail.com>
2025-12-07 02:08:33 +0600

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import Home from "./Page/Home/Home";
 import Root from "./Layout/Root";
 import Skills from "./Page/Skills Page/SkillsPage";
 import ProjectsCard from "./Page/My Projects/ProjectsCard";
-<<<<<<< HEAD
 import BlogsCard from "./Page/Home/HomeComponents/BlogsCard";
-=======
->>>>>>> f2942c2959d9ba4a7028f128682967e8beb1d885
 import Client from "./Page/Client/Client";
 import Pricing from "./Page/Pricing/Pricing";
 import ContactMe from "./Page/Contact Me Page/ContactMePage";
@@ -19,10 +16,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="skills" element={<Skills></Skills>}></Route>
           <Route path="myprojects" element={<ProjectsCard></ProjectsCard>}></Route>
-<<<<<<< HEAD
           <Route path="myblogs" element={<BlogsCard></BlogsCard>}></Route>
-=======
->>>>>>> f2942c2959d9ba4a7028f128682967e8beb1d885
           <Route path="client" element={<Client></Client>}></Route>
           <Route path="pricing" element={<Pricing></Pricing>}></Route>
           <Route path="contactme" element={<ContactMe></ContactMe>}></Route>
