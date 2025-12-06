4a1f773cbf2986b50a020f5280c2a85b3a1dbf27
Asim <asimankon@gmail.com>
2025-12-07 02:11:44 +0600

    Updated code

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -3,10 +3,7 @@ import Hero from './HomeComponents/Hero';
 import Skills from './HomeComponents/Skills';
 import Certification from './HomeComponents/Certification';
 import ProjectsCard from '../My Projects/ProjectsCard';
-<<<<<<< HEAD
 import Blogs from '../My Blogs/Blogs';
-=======
->>>>>>> f2942c2959d9ba4a7028f128682967e8beb1d885
 import Client from '../Client/Client';
 import Pricing from '../Pricing/Pricing';
 import ContactMe from './HomeComponents/ContactMe';
@@ -19,10 +16,7 @@ const Home = () => {
             <Certification />
             <Skills />
             <ProjectsCard />
-<<<<<<< HEAD
             <Blogs />
-=======
->>>>>>> f2942c2959d9ba4a7028f128682967e8beb1d885
             <Client />
             <Pricing />
             <ContactMe />
